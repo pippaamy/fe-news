bc3f544062b7b932146b5f016ef616e9d51ce93f
pippaamy <pippa_austin@live.com>
2024-02-14 17:23:31 +0000

    path incorrect

--- src/components/Comments.jsx
+++ src/components/Comments.jsx
@@ -2,7 +2,7 @@ import { useContext, useEffect, useState } from "react";
 import { deleteComment, getComments } from "../api";
 import { useParams } from "react-router-dom";
 import dateFormat from "dateformat";
-import NewComment from "./NewComment";
+import NewComment from "./newComment";
 import CommentBox from "./CommentBox";
 import { UserContext } from "./User";
 import CommentVotes from "./CommentVotes";
